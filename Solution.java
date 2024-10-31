b6653ae17a64dd44940fbdc02782fec1bc534f85
Penny Hardaway <475215536@qq.com>
2024-10-31 14:29:33 +0800

    add Junit

---
+++ Solution.java
@@ -0,0 +1,9 @@
+
+import java.util.List;
+
+class Solution {
+    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
+        return 1;
+    }
+
+}
---
+++ SolutionTest.java
@@ -0,0 +1,10 @@
+import org.junit.Test;
+
+public class SolutionTest {
+    @Test
+    public void testCq() {
+        Solution s = new Solution();
+        long a = s.minimumTotalDistance(null,null);
+        System.out.println(a);
+    }
+}
