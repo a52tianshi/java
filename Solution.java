206556d095f33de604c8abc1505b234931b60189
Penny Hardaway <475215536@qq.com>
2024-11-02 12:41:04 +0800

    add ignore /out/

--- Solution.java
+++ Solution.java
@@ -1,9 +1,28 @@
-
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
 
 class Solution {
-    public long minimumTotalDistance(List<Integer> robot, int[][] factory) {
-        return 1;
+    public boolean isCircularSentence(String sentence) {
+        if (sentence == null) {
+            return false;
+        }
+        ArrayList<String> words = new ArrayList<String>();
+        Collections.addAll(words, sentence.split(" "));
+        for (int i = 0; i < words.size(); i++) {
+            if (i != 0) {
+                String lastWord = words.get(i - 1);
+                String firstWord = words.get(i);
+                if (lastWord.charAt(lastWord.length() - 1) != firstWord.charAt(0)) {
+                    return false;
+                }
+            } else {
+                String lastWord = words.get(words.size() - 1);
+                String firstWord = words.get(i);
+                if (lastWord.charAt(lastWord.length() - 1) != firstWord.charAt(0)) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
-
 }
--- SolutionTest.java
+++ SolutionTest.java
@@ -4,7 +4,13 @@ public class SolutionTest {
     @Test
     public void testCq() {
         Solution s = new Solution();
-        long a = s.minimumTotalDistance(null,null);
+        boolean a = s.isCircularSentence("Leetcode is cool");
+        boolean b = s.isCircularSentence("leetcode exercises sound delightful");
         System.out.println(a);
+        System.out.println(b);
+        boolean c = s.isCircularSentence("leetcode eool");
+        System.out.println(c);
+        boolean d = s.isCircularSentence("Leetcode eool");
+        System.out.println(d);
     }
 }
