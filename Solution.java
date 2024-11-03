f1ea211b5277a55e068c7fe843159f863d271228
Penny Hardaway <475215536@qq.com>
2024-11-03 22:23:20 +0800

    add rotateString

--- Solution.java
+++ Solution.java
@@ -1,28 +1,21 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
-    public boolean isCircularSentence(String sentence) {
-        if (sentence == null) {
+    public boolean rotateString(String s, String goal) {
+        int sLen = s.length();
+        if (sLen != goal.length()) {
             return false;
         }
-        ArrayList<String> words = new ArrayList<String>();
-        Collections.addAll(words, sentence.split(" "));
-        for (int i = 0; i < words.size(); i++) {
-            if (i != 0) {
-                String lastWord = words.get(i - 1);
-                String firstWord = words.get(i);
-                if (lastWord.charAt(lastWord.length() - 1) != firstWord.charAt(0)) {
-                    return false;
-                }
-            } else {
-                String lastWord = words.get(words.size() - 1);
-                String firstWord = words.get(i);
-                if (lastWord.charAt(lastWord.length() - 1) != firstWord.charAt(0)) {
-                    return false;
+        for (int i = 0; i < sLen; i++) {
+            var match = true;
+            for (int j = 0; j < sLen; j++) {
+                if (s.charAt((i + j) % sLen) != goal.charAt(j)) {
+                    match = false;
+                    break;
                 }
             }
+            if (match) {
+                return true;
+            }
         }
-        return true;
+        return false;
     }
 }
--- SolutionTest.java
+++ SolutionTest.java
@@ -4,13 +4,7 @@ public class SolutionTest {
     @Test
     public void testCq() {
         Solution s = new Solution();
-        boolean a = s.isCircularSentence("Leetcode is cool");
-        boolean b = s.isCircularSentence("leetcode exercises sound delightful");
-        System.out.println(a);
-        System.out.println(b);
-        boolean c = s.isCircularSentence("leetcode eool");
-        System.out.println(c);
-        boolean d = s.isCircularSentence("Leetcode eool");
-        System.out.println(d);
+        assert s.rotateString("abcde", "cdeab") == true;
+        assert s.rotateString("abcde", "abced") == false;
     }
 }
